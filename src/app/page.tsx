ee0bb810c5661df93ce2bd795047b17fa8d3f889
InsaneCoder <kemdavid10@gmail.com>
2025-06-20 22:16:43 +0800

    feat: configured tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home(){
   return (
     <div>
-      <h1 className="text-amber-200">Hello</h1>
+      <h1 className="bg-mint-500">Hello</h1>
       <button className="font-bold bg-amber-100 hover:bg-blue-300">Tambayan Button</button>
     </div>
   )
