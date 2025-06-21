78e6e4681a66486fa42fda928f86feb0ecf35978
InsaneCoder <kemdavid10@gmail.com>
2025-06-21 20:29:37 +0800

    feat: added example for icons using svg

---
+++ src/app/components/icons/index.ts
@@ -0,0 +1,8 @@
+// export { PlusIcon } from './PlusIcon';
+// export { UserIcon } from './UserIcon';
+// export { SettingsIcon } from './SettingsIcon';
+
+export {TestIcon} from './TestIcon'
+
+// When used at a page
+// import { PlusIcon, UserIcon } from '@/components/icons';
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 
-import {Button} from "../components/ui/button"
+import { Button } from "../components/ui/button"
+import { TestIcon } from "../components/icons";
 
 export default function Test() {
   return (
-  <div>
-    <Button onClick={() => console.log("Button Clicked")}>Button Text</Button>
-  </div>
+    <div>
+      <Button onClick={() => console.log("Button Clicked")}>
+        <TestIcon/>
+        Create Post
+      </Button>
+    </div>
   );
 }
