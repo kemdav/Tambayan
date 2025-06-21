aecea00d090483414a1de699945a8fcf52bbfcc8
InsaneCoder <kemdavid10@gmail.com>
2025-06-21 17:40:12 +0800

    feat: fixed the duplicate and buggy component folder

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import Button from "../components/ui/button"
+import {Button} from "../components/ui/button"
 
 export default function Test() {
   return (
   <div>
-    <Button variant="" size="" onClick={() => console.log("Button Clicked")}>Button Text</Button>
+    <Button onClick={() => console.log("Button Clicked")}>Button Text</Button>
   </div>
   );
 }
