1599a177f2455494ced4fbd170b9cfe06ae877be
InsaneCoder <kemdavid10@gmail.com>
2025-06-22 17:04:13 +0800

    feat: Added navigation button variant

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -5,17 +5,17 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "select-none cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "select-none cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 rounded-md",
         destructive:
-          "bg-action-error text-white shadow-xs hover:bg-action-error/90 focus-visible:ring-action-error/20 dark:focus-visible:ring-action-error/40 dark:bg-action-error/60",
-        outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-        link: "text-info underline-offset-4 hover:underline",
+          "bg-action-error text-white shadow-xs hover:bg-action-error/90 focus-visible:ring-action-error/20 dark:focus-visible:ring-action-error/40 dark:bg-action-error/60 rounded-md",
+        navigation:
+          "bg-primary text-primary-foreground shadow-xs hover:bg-shade-1",
+        link: "text-info underline-offset-4 hover:underline rounded-md",
       },
       size: {
         default: "h-9 px-4 py-1 has-[>svg]:px-3",
--- src/app/test/button-test/page.tsx
+++ src/app/test/button-test/page.tsx
@@ -5,27 +5,59 @@ import { TestIcon } from "../../components/icons";
 
 export default function Test() {
   return (
-    <div className="flex justify-center space-x-10 mt-10">
-      <Button onClick={() => console.log("Button Clicked")}>
-        Default
-      </Button>
+    <div className="grid grid-flow-col grid-rows-4 grid-cols-1 gap-4 bg-blue-100 p-10">
+      <div className="flex justify-center  bg-tint-4 rounded-2xl">
+        <Button onClick={() => console.log("Button Clicked")}>
+          Default
+          </Button>
 
-      <Button onClick={() => console.log("Button Clicked")}>
-        <TestIcon/>
-        With Icon
-      </Button>
+          <Button onClick={() => console.log("Button Clicked")}>
+            <TestIcon/>
+            With Icon
+          </Button>
 
-      <Button onClick={() => console.log("Button Clicked")} variant="destructive">
-        Destructive
-      </Button>
+          <Button onClick={() => console.log("Button Clicked")} variant="destructive">
+            Destructive
+          </Button>
 
-      <Button onClick={() => console.log("Button Clicked")} variant="link">
-        Link
-      </Button>
+          <Button onClick={() => console.log("Button Clicked")} variant="link">
+            Link
+          </Button>
 
-      <Button onClick={() => console.log("Button Clicked")} variant="default" disabled={true}>
-       Disabled
-      </Button>
+          <Button onClick={() => console.log("Button Clicked")} variant="default" disabled={true}>
+          Disabled
+          </Button>
+      </div>
+      <div className="grid grid-flow-col row-span-3 grid-rows-4 grid-cols-5 gap-4">
+        <div className="flex flex-col row-span-3 bg-amber-50">
+          <Button onClick={() => console.log("Button Clicked")} >
+          Default
+          </Button>
+
+          <Button onClick={() => console.log("Button Clicked")}>
+            <TestIcon/>
+            With Icon
+          </Button>
+
+          <Button onClick={() => console.log("Button Clicked")} variant="destructive">
+            Destructive
+          </Button>
+
+          <Button onClick={() => console.log("Button Clicked")} variant="link">
+            Link
+          </Button>
+
+          <Button onClick={() => console.log("Button Clicked")} variant="navigation">
+            <TestIcon/>
+            Navigation
+          </Button>
+
+          <Button onClick={() => console.log("Button Clicked")} variant="default" disabled={true}>
+          Disabled
+          </Button>
+        </div>
+        <div className="row-span-3 col-span-4 bg-tint-4 rounded-2xl"></div>  
+      </div>
     </div>
   );
 }
