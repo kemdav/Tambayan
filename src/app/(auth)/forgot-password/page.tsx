c1a65f76a6febda134bc62a777bfd21ebf2a5d6e
InsaneCoder <kemdavid10@gmail.com>
2025-06-23 23:28:03 +0800

    feat: added initial pages routing

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="bg-mint-500">Forgot Password Page</h1>
+    </div>
+  );
+}
---
+++ src/app/(student-dashboard)/student/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="bg-mint-500">Register Page</h1>
+    </div>
+  );
+}
