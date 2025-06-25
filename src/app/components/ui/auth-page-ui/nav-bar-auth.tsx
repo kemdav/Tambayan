f72f14ba8d5854d41e161a4671745649caaf199c
Plep <kemdavid10@gmail.com>
2025-06-25 12:25:42 +0800

    fix: fixed hydration issue

--- src/app/components/ui/auth-page-ui/nav-bar-auth.tsx
+++ src/app/components/ui/auth-page-ui/nav-bar-auth.tsx
@@ -1,16 +1,38 @@
-import { TambayanIcon } from '../../icons';
-import {Button} from "@/app/components/ui/button";
+"use client";
+import { TambayanIcon } from "../../icons";
+import { Button } from "@/app/components/ui/button";
+import { useRouter } from "next/navigation";
 
-
-export default function AuthNavBar(){
-    return <div className="bg-secondary-muted-sage grid grid-cols-2 p-3">
-        <div>
-            <TambayanIcon size={40}/>
-        </div>
-        <div className='flex flex-row-reverse gap-10 mr-20'>
-            <Button variant="navigation" className='text-neutral-ivory-white bg-secondary-muted-sage rounded-4xl'>Register</Button>
-            <Button variant="navigation" className='bg-primary-moss-green text-neutral-ivory-white rounded-4xl'>Login</Button>
-            <Button variant="navigation" className='rounded-4xl bg-secondary-muted-sage'>Home</Button>
-        </div>
-    </div>;
+export default function AuthNavBar() {
+  const router = useRouter();
+  return (
+    <div className="bg-secondary-muted-sage grid grid-cols-2 p-3">
+      <div>
+        <TambayanIcon size={40} />
+      </div>
+      <div className="flex flex-row-reverse gap-10 mr-20">
+        <Button
+          variant="navigation"
+          className="text-neutral-ivory-white bg-secondary-muted-sage rounded-4xl"
+          onClick={() => router.push("/register")}
+        >
+          Register
+        </Button>
+        <Button
+          variant="navigation"
+          className="bg-primary-moss-green text-neutral-ivory-white rounded-4xl"
+          onClick={() => router.push("/login")}
+        >
+          Login
+        </Button>
+        <Button
+          variant="navigation"
+          className="rounded-4xl bg-secondary-muted-sage"
+          onClick={() => router.push("/")}
+        >
+          Home
+        </Button>
+      </div>
+    </div>
+  );
 }
