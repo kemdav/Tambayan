042ca42d3e74399d270bea70fad8e903fc85917e
InsaneCoder <kemdavid10@gmail.com>
2025-06-29 23:36:20 +0800

    feat: handle logic for the parent

--- src/app/test/wiki-list-test/page.tsx
+++ src/app/test/wiki-list-test/page.tsx
@@ -2,28 +2,31 @@
 
 import { FaBook, FaUser, FaCogs } from "react-icons/fa";
 import WikiListComponent from "@/app/components/ui/general/wiki-list-component";
+import { useRouter } from "next/navigation";
 
-const dummyCards = [
+const wikiData = [
   {
     id: "1",
     title: "Introduction",
     icon: <FaBook />,
-    onClick: () => console.log("Introduction clicked"),
   },
   {
     id: "2",
     title: "How to Join",
     icon: <FaUser />,
-    onClick: () => console.log("How to Join clicked"),
   },
   {
     id: "3",
     title: "Settings & Tools",
     icon: <FaCogs />,
-    onClick: () => console.log("Settings & Tools clicked"),
   },
 ];
 
 export default function WikiListTest() {
-  return <WikiListComponent textcolor="text-green-700" cards={dummyCards} />;
+  const router = useRouter();
+  const cardsWithNavigation = wikiData.map(card => ({
+    ...card,
+    onClick: () => router.push(`/wiki/${card.id}`),
+  }));
+  return <WikiListComponent textcolor="text-green-700" cards={cardsWithNavigation} />;
 }
